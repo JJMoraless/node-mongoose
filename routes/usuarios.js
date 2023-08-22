bcfa4933beff7aec21ce16a033ca8e2e02f5ed69
JJMoraless <jjmorales.dev@gmail.com>
2023-08-21 20:42:09 -0500

    feat: :sparkles: endpoint crud usuario con proteccion de ruta delete

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import { check } from "express-validator";
+import validarCampos from "../middlewares/validar-campos.js";
 
 import {
   usuariosGet,
@@ -8,11 +10,57 @@ import {
   usuariosPatch,
 } from "../controllers/usuarios.js";
 
+import {
+  emailExiste,
+  esRoleValido,
+  existeUsuarioPorId,
+} from "../helpers/db-validators.js";
+
+import { validarJwt } from "../middlewares/validar-jwt.js";
+import { tieneRol } from "../middlewares/validar-roles.js";
+
 const router = Router();
 router.get("/", usuariosGet);
-router.put("/:id", usuariosPut);
-router.post("/", usuariosPost);
-router.delete("/", usuariosDelete);
+
+router.put(
+  "/:id",
+  [
+    check("id", "No es un ID válido").isMongoId(),
+    check("id").custom(existeUsuarioPorId),
+    check("rol").custom(esRoleValido),
+    validarCampos,
+  ],
+  usuariosPut
+);
+
+router.post(
+  "/",
+  [
+    check("nombre", "El nombre es obligatorio").not().isEmpty(),
+    check("password", "El password debe de ser más de 6 letras").isLength({
+      min: 6,
+    }),
+    check("correo", "El correo no es válido").isEmail(),
+    check("correo").custom(emailExiste),
+    // check('rol', 'No es un rol válido').isIn(['ADMIN_ROLE','USER_ROLE']),
+    check("rol").custom(esRoleValido),
+    validarCampos,
+  ],
+  usuariosPost
+);
+
+router.delete(
+  "/:id",
+  [
+    validarJwt,
+    tieneRol("ADMIN_ROLE", "USUARIO_ROLE"),
+    check("id", "No es un id valido").isMongoId(),
+    check("id").custom(existeUsuarioPorId),
+    validarCampos,
+  ],
+  usuariosDelete
+);
+
 router.patch("/", usuariosPatch);
 
 export default router;
