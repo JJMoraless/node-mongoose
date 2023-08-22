0c2d0a5da23c6ffe020552aedd6d712171c2b9d7
JJMoraless <jjmorales.dev@gmail.com>
2023-08-21 20:44:03 -0500

    feat: :sparkles: instancia del server

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-import "dotenv/config"
-import Server from './models/server.js';
-
-
+import "dotenv/config";
+import Server from "./models/server.js";
 
 const server = new Server();
 server.listen();
